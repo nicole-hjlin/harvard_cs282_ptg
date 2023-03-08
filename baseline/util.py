9f8cf063ff282f7509e4dbba2d0fd963f67373be
danwley <d.w.ley@hotmail.com>
2023-03-08 00:24:32 -0500

    implemented voting and abstention in predict_ensemble

--- baseline/util.py
+++ baseline/util.py
@@ -2,6 +2,7 @@ from typing import Callable
 import torch
 from torch import nn
 from torch.utils.data import Dataset
+from scipy.stats import binom
 from dataclasses import dataclass
 import wandb
 
@@ -47,9 +48,21 @@ def predict_ensemble(
     a: float,
     x: torch.Tensor,
 ):
-    y_pred = sum(net.eval()(x) for net in g)
-    p = -1 # idk what the paper means here
+    # Assuming model(x) returns softmax vector of length no. classes
+    softmax_preds = torch.stack([model(x)[0] for model in g])
+    counts = torch.bincount(softmax_preds.argmax(1))
+
+    # Compute top 2 classes (c) and their frequencies (n)
+    # E.g. c = [5, 3] and n = [22, 20]
+    # (classes 5 and 3 recieved 20 and 22 votes)
+    n, c = counts.topk(2)
+    n_a, n_b = n.tolist()
+    c_a, c_b = c.tolist()  # following Appendix notation
+
+    # Compute binomial probability of n_a,
+    # assuming equal chance between c_a and c_b
+    p = binom.pmf(n_a, n_a+n_b, 0.5)
     if p < a:
-        return y_pred.argmax(-1)
+        return c_a
     else:
         return None
