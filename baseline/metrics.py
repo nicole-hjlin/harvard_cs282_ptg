9dddc4de6b36b5dfea7bd2aac6af05fd0d3ad612
danwley <d.w.ley@hotmail.com>
2023-03-10 20:55:25 -0500

    camera ready metrics baby

--- baseline/metrics.py
+++ baseline/metrics.py
@@ -1,3 +1,5 @@
+"""Metrics for evaluating the performance of a model or ensemble of models"""
+
 import torch
 from torch import nn
 from util import select_preds, convert_to_numpy
@@ -9,11 +11,12 @@ def compute_preds(
     x: torch.Tensor | np.ndarray,
     return_numpy: bool = False,
 ):
-    # May run into memory issues in future
+    """Compute predictions for each model in g for each input in x"""
+
+    # May run into memory issues for larger datasets
     preds = torch.zeros((len(g), x.shape[0]), dtype=torch.int)
     for i, model in enumerate(tqdm(g)):
-        # predict function is flexible to type(x)
-        preds[i] = model.predict(x, return_numpy=False)
+        preds[i] = model.predict(x, return_numpy=False) # flexible to type(x)
 
     # return shape is (no. models, no.inputs)
     # may want to flatten in case of single input
@@ -25,37 +28,44 @@ def compute_accuracies(
     preds: torch.Tensor | np.ndarray,
     y: torch.Tensor | np.ndarray,
 ):
-    # Convert to numpy if a tensor
+    """Compute accuracy for each model for each prediction"""
+
+    # Convert to numpy if tensors
     preds = convert_to_numpy(preds)
     y = convert_to_numpy(y)
-        
+
     return (preds==y).mean(axis=1)*100
+
 
 def compute_abstention_rate(
     preds: torch.Tensor | np.ndarray,
 ):
+    """Compute the percentage of inputs for which an ensemble abstains"""
+
     return np.mean(preds==np.inf, axis=1)*100
+
 
 def compute_disagreement(
     preds: torch.Tensor | np.ndarray,
 ):
     """Proportion of inputs with disagreement between at least one pair of models
-    For each input in x_test, if ALL predictions ARE NOT the same, assign 1 to that input
-    Return "mean of portion of test data with p_flip > 0"
-    Note that we're not computing the probability of flip between pairwise models
-    For now, we're just computing if the probability is greater than 0
-    i.e. at least one model has a different prediction to all the others"""
+    For each input, if all predictions are not the same between models,
+    assign 1 to that input, else assign 0 since all models agree
+    preds is a 2D array of size (no. models, no. inputs)
+    or a 1D array, in which case 0.0 is returned"""
+
     # Special case for predictions from a single model
     if len(preds.shape)==1:
         return 0.0  # no disagreement
-    
+
     # Convert to numpy if a tensor
     preds = convert_to_numpy(preds)
 
     # For each input, check if any models predict different to the first model
     p_flip_positive = np.any(preds!=preds[0], axis=0)  # size is no. inputs
-    
+
     return p_flip_positive.mean()
+
 
 def compute_ensemble_predictions(
     ensemble_size: int,
@@ -63,6 +73,9 @@ def compute_ensemble_predictions(
     preds: torch.Tensor | np.ndarray,
     ensemble_method: str = 'selective',
 ):
+    """Compute predictions for each selective ensemble
+    preds is a 2D array of size (no. models, no. inputs)"""
+
     # Convert to numpy if a tensor
     preds = convert_to_numpy(preds)
 
@@ -79,47 +92,23 @@ def compute_ensemble_predictions(
 
         # Combine predictions
         ensemble_preds[i] = select_preds(ensemble_model_preds, ensemble_method=ensemble_method)
-    
+
     # Return predictions for each selective ensemble
     return ensemble_preds
 
+
 def p_flip_ensemble(
-    ensembles: list[list[nn.Module]],  # list of g, where g is list of singletons OR list of ensemble class
-    X_test: torch.Tensor,  # multiple inputs
+    ensembles: list[list[nn.Module]],
+    X_test: torch.Tensor,
     a: float,
 ):
-    # Ideally we just have one function that takes in a list of models
-    # So we might need a model class for the selective ensemble
-    # We also need a function for the standard ensemble
+    """Compute the probability of flip for each ensemble in set of ensembles
+    ensembles is a list of g, where g is a list of singletons OR list of ensemble class"""
 
-    # Alternatively, modify util/select_preds() to process a whole array of inputs instead of just 1
-    # As the inputs above suggest
-    # select_preds should return array of size no. inputs, with nan value for abstention
-    # e.g. size of preds is 10 x 10000 for FMNIST (10 ensembles, 10000 test points)
+    # Compute predictions for each ensemble
     preds = torch.stack([select_preds(g, a, X_test) for g in ensembles])
     p_flip_positive = torch.any(preds!=preds[0], dim=1).to(torch.float)
 
-    # We'll assume that if all ensembles abstain for one particular input, all predictions are the same for that input
-
+    # We'll assume that if all ensembles abstain for one particular input,
+    # all predictions are the same for that input
     return p_flip_positive.mean()
-
-def SSIM(
-    grads1: torch.Tensor,
-    grads2: torch.Tensor,
-):
-    # Structural Similarity (SSIM), varies from -1 to 1
-    # For FMNIST, this replaces Spearman's rank
-    # They use: https://scikit-image.org/docs/stable/auto_examples/transform/plot_ssim.html
-
-    # For now, let's implement it for 2 gradient vectors that are the "feature attributions" for a single input
-
-    return -1
-
-
-def pearsonr(
-    grads1: torch.Tensor,
-    grads2: torch.Tensor,
-):
-    # I think we'll literally just use:
-    # https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.pearsonr.html
-    return -1
