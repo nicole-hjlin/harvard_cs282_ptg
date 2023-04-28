f6f9ff5a18d879ddc390ccd5dfdcb2cbff213879
danwley <d.w.ley@hotmail.com>
2023-04-28 00:45:45 -0400

    reformatted

--- datasets/tabular.py
+++ datasets/tabular.py
@@ -13,9 +13,9 @@ from sklearn.model_selection import train_test_split
 from torch.utils.data import Dataset, Subset
 from torchvision.transforms import ToTensor
 from util import State, convert_to_tensor, get_optimizer
+from modconn import curves
 from .german import preprocess_german
 from typing import Tuple
-from modconn import curves
 
 
 # Would be cleaner if these were included in their respective files actually
@@ -25,98 +25,6 @@ preprocess_funcs = {'german': preprocess_german,
                     'heloc': None}
 layers = {'german': [128, 64, 16],
           'heloc': [128, 64, 16]}
-
-class TabularSubset(Subset):
-    def __init__(self, dataset, indices):
-        super().__init__(dataset, indices)
-        
-        # Assign the attributes from the original dataset to the new subset
-        self.name = dataset.name
-        self.data = dataset.data[indices]
-        self.labels = dataset.labels[indices]
-
-class TabularDataset(Dataset):
-    """
-    A dataset for tabular data, instantiated for a specific dataset as a train or test set.
-    """
-    def __init__(self, name, download_url=None, train=True, preprocess_func=None,
-                 sep=" +", transform=ToTensor()):
-        self.name = name
-        self.download_url = download_url
-        self.train = train
-        self.preprocess_func = preprocess_func
-        self.sep = sep
-        self.transform = transform
-        self.data_dir = f'datasets/data/{name}'
-        self.train_file = f'{self.data_dir}/{name}_train.csv'
-        self.test_file = f'{self.data_dir}/{name}_test.csv'
-
-        if download_url is not None:
-            self.download_and_process()
-
-        self.data, self.labels = self.load_data()
-
-    def download_and_process(self):
-        """
-        If the directory doesn't exist, make it.
-        If the train or test file doesn't exist,
-        download the dataset, preprocess it,
-        and split it into train and test sets,
-        Save them as CSV files.
-        """
-        makedirs(self.data_dir, exist_ok=True)
-
-        if not path.isfile(self.train_file) or not path.isfile(self.test_file):
-            # Download the dataset
-            response = requests.get(self.download_url, timeout=60)
-            response.raise_for_status()
-            raw_data = pd.read_csv(io.StringIO(response.text),
-                                   sep=self.sep,
-                                   engine="python",
-            )
-
-            # Preprocess the dataset if a preprocessing function is provided
-            raw_data = self.preprocess_func(raw_data)
-
-            # Split the dataset into train and test sets
-            train_data, test_data = train_test_split(raw_data, test_size=0.2, random_state=0)
-
-            # Save the train and test sets as CSV files
-            train_data.to_csv(self.train_file, index=False)
-            test_data.to_csv(self.test_file, index=False)
-
-    def load_data(self):
-        """Load the train or test set into a pandas DataFrame and convert to tensors."""
-        data_file = self.train_file if self.train else self.test_file
-        data = pd.read_csv(data_file)
-
-        # Assuming the labels column is called 'label'
-        labels = data['label']
-        data = data.drop(columns=['label'])
-
-        # Retrieve values as numpy arrays
-        data = data.values
-        labels = labels.values
-
-        # Convert the remaining data to tensors
-        data = torch.tensor(data, dtype=torch.float)
-        labels = torch.tensor(labels, dtype=torch.long)
-
-        return data, labels
-
-    def __len__(self):
-        """Return the length of the dataset. Necessary for PyTorch's DataLoader."""
-        return len(self.data)
-
-    def __getitem__(self, idx):
-        """Return the sample at index idx. Necessary for PyTorch's DataLoader."""
-        if torch.is_tensor(idx):
-            idx = idx.tolist()
-        
-        sample = self.data[idx]
-        label = self.labels[idx]
-        return sample, label
-
 
 def learning_pipeline(S: State) -> nn.Module:
     """Learning pipeline for tabular datasets
@@ -222,6 +130,99 @@ def load_tabular_dataset(name: str) -> Tuple[TabularDataset, TabularDataset]:
 
     # Return train/test sets
     return trainset, testset
+
+class TabularSubset(Subset):
+    def __init__(self, dataset, indices):
+        super().__init__(dataset, indices)
+        
+        # Assign the attributes from the original dataset to the new subset
+        self.name = dataset.name
+        self.data = dataset.data[indices]
+        self.labels = dataset.labels[indices]
+
+class TabularDataset(Dataset):
+    """
+    A dataset for tabular data, instantiated for a specific dataset as a train or test set.
+    """
+    def __init__(self, name, download_url=None, train=True, preprocess_func=None,
+                 sep=" +", transform=ToTensor()):
+        self.name = name
+        self.download_url = download_url
+        self.train = train
+        self.preprocess_func = preprocess_func
+        self.sep = sep
+        self.transform = transform
+        import os, sys
+        print(os.getcwd())
+        self.data_dir = f'datasets/data/{name}'
+        self.train_file = f'{self.data_dir}/{name}_train.csv'
+        self.test_file = f'{self.data_dir}/{name}_test.csv'
+
+        if download_url is not None:
+            self.download_and_process()
+
+        self.data, self.labels = self.load_data()
+
+    def download_and_process(self):
+        """
+        If the directory doesn't exist, make it.
+        If the train or test file doesn't exist,
+        download the dataset, preprocess it,
+        and split it into train and test sets,
+        Save them as CSV files.
+        """
+        makedirs(self.data_dir, exist_ok=True)
+
+        if not path.isfile(self.train_file) or not path.isfile(self.test_file):
+            # Download the dataset
+            response = requests.get(self.download_url, timeout=60)
+            response.raise_for_status()
+            raw_data = pd.read_csv(io.StringIO(response.text),
+                                   sep=self.sep,
+                                   engine="python",
+            )
+
+            # Preprocess the dataset if a preprocessing function is provided
+            raw_data = self.preprocess_func(raw_data)
+
+            # Split the dataset into train and test sets
+            train_data, test_data = train_test_split(raw_data, test_size=0.2, random_state=0)
+
+            # Save the train and test sets as CSV files
+            train_data.to_csv(self.train_file, index=False)
+            test_data.to_csv(self.test_file, index=False)
+
+    def load_data(self):
+        """Load the train or test set into a pandas DataFrame and convert to tensors."""
+        data_file = self.train_file if self.train else self.test_file
+        data = pd.read_csv(data_file)
+
+        # Assuming the labels column is called 'label'
+        labels = data['label']
+        data = data.drop(columns=['label'])
+
+        # Retrieve values as numpy arrays
+        data = data.values
+        labels = labels.values
+
+        # Convert the remaining data to tensors
+        data = torch.tensor(data, dtype=torch.float)
+        labels = torch.tensor(labels, dtype=torch.long)
+
+        return data, labels
+
+    def __len__(self):
+        """Return the length of the dataset. Necessary for PyTorch's DataLoader."""
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        """Return the sample at index idx. Necessary for PyTorch's DataLoader."""
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+        
+        sample = self.data[idx]
+        label = self.labels[idx]
+        return sample, label
 
 class TabularModel(nn.Module):
     """Tabular model for binary classification"""
