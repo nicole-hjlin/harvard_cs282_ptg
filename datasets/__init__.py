141b249a10ae5ae3a47391684b20132006825ae5
danwley <d.w.ley@hotmail.com>
2023-04-27 23:42:22 -0400

    remove commented tabular dataset class

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,4 +1,4 @@
-"""Dataset module to load (and download) datasets. Reminder to put this in a package for AI4LIFE."""
+"""Dataset module to load (and download) datasets."""
 from .fmnist import load_fmnist_dataset
 from .tabular import load_tabular_dataset
 
@@ -9,38 +9,3 @@ def load_dataset(name):
         return load_tabular_dataset(name)
     else:
         raise ValueError(f"Unknown dataset: {name}")
-
-
-# class TabularDataset(Dataset):
-#     def __init__(self,
-#         source_url: str,
-#         download_path: str,
-#         sep: str,
-#         preprocess: Callable[[pd.DataFrame], Tuple[torch.Tensor, torch.Tensor]],
-#     ):
-#         # Create the directory if it doesn't exist
-#         makedirs(path.dirname(download_path), exist_ok=True)
-        
-#         # Check if the file doesn't exist, then download and save it
-#         if not path.isfile(download_path):
-#             with open(download_path, 'wb') as f:
-#                 r = requests.get(source_url, allow_redirects=True)
-#                 f.write(r.content)
-#             source_url = download_path
-
-#         df = pd.read_csv(
-#             source_url,
-#             sep=sep,
-#             engine="python",
-#         )
-
-#         x, y = preprocess(df)
-
-#         self.x = x
-#         self.y = y
-
-#     def __len__(self):
-#         return len(self.x)
-
-#     def __getitem__(self, idx):
-#         return self.x[idx], self.y[idx]
