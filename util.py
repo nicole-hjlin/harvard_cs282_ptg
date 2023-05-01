0e772111ba36b084b58d8ae446d7e15bf6d1693a
danwley <d.w.ley@hotmail.com>
2023-04-30 21:52:53 -0400

    move get_statistics to util.py

--- util.py
+++ util.py
@@ -46,3 +46,25 @@ def convert_to_numpy(arr):
 
     # Return numpy array
     return arr.numpy() if isinstance(arr, torch.Tensor) else arr
+
+def get_statistics(model_idx, method, directory):
+    if method == 'average':
+        grads = np.array([np.load(f'{directory}/grads_{idx}.npy') for idx in model_idx]).mean(axis=0)
+        logits = np.array([np.load(f'{directory}/logits_{idx}.npy') for idx in model_idx])
+        preds = logits.mean(axis=0).argmax(axis=1)
+    elif method == 'majority':
+        logits = np.array([np.load(f'{directory}/logits_{idx}.npy') for idx in model_idx])
+        grads = np.array([np.load(f'{directory}/grads_{idx}.npy') for idx in model_idx])
+        preds = np.zeros(logits.shape[1], dtype=int)
+        preds[logits.argmax(2).mean(axis=0) >= 0.5] = 1
+        majority_votes = (logits.argmax(axis=2) == preds)
+        num_majority_votes = majority_votes.sum(axis=0)
+        selected_grads = np.where(majority_votes[:, :, None], grads, 0)
+        grads = selected_grads.sum(axis=0) / num_majority_votes[:, None]
+    elif method == 'perturb':
+        grads = np.array([np.load(f'{directory}/grads_perturb_{idx}.npy') for idx in model_idx]).mean(axis=0)
+        logits = np.array([np.load(f'{directory}/logits_perturb_{idx}.npy') for idx in model_idx])
+        preds = logits.mean(axis=0).argmax(axis=1)
+    else:
+        raise ValueError(f'Invalid method: {method}')
+    return grads, preds
