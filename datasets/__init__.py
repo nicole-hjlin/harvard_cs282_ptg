e5cb112f42342ff3048fac22d6627b71f365a312
danwley <d.w.ley@hotmail.com>
2023-05-08 10:32:27 -0400

    load default credit

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,10 +1,12 @@
 import datasets.fmnist as fmnist
 import datasets.tabular as tabular
 
+_tabular_datasets = ['german', 'adult', 'heloc', 'default']
+
 def load_dataset(name):
     if name == 'fmnist':
         return fmnist.load_fmnist_dataset()
-    elif name in ['german', 'adult', 'heloc']:
+    elif name in _tabular_datasets:
         return tabular.load_tabular_dataset(name)
     else:
         raise ValueError(f"Unknown dataset: {name}")
@@ -13,7 +15,7 @@ def get_model_class(name: str):
     """Returns a model class by dataset name"""
     if name == 'fmnist':
         model_class = fmnist.LeNet5
-    elif name in ['german', 'adult', 'heloc']:
+    elif name in _tabular_datasets:
         model_class = tabular.TabularModel
     else:
         raise ValueError(f'Unknown dataset name: {name}')
@@ -23,7 +25,7 @@ def get_curve_class(name: str):
     """Returns a curve model class by dataset name"""
     if name == 'fmnist':
         curve_class = fmnist.LeNet5Curve
-    elif name in ['german', 'adult', 'heloc']:
+    elif name in _tabular_datasets:
         curve_class = tabular.TabularModelCurve
     else:
         raise ValueError(f'Unknown dataset name: {name}')
@@ -33,7 +35,7 @@ def get_learning_pipeline(name: str):
     """Returns a learning pipeline by dataset name"""
     if name == 'fmnist':
         learning_pipeline = fmnist.learning_pipeline
-    elif name in ['german', 'adult', 'heloc']:
+    elif name in _tabular_datasets:
         learning_pipeline = tabular.learning_pipeline
     else:
         raise ValueError(f'Unknown dataset name: {name}')
