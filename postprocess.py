3dc988543afa7c84f3403adf1e8d5f134f50467e
danwley <d.w.ley@hotmail.com>
2023-05-14 20:31:16 -0400

    removed fmnist and get_curve_class/get_curve_model convention

--- postprocess.py
+++ postprocess.py
@@ -6,8 +6,7 @@ import torch
 import numpy as np
 import datasets
 import curves
-from datasets.tabular import TabularModel, TabularModelPerturb
-from datasets import get_model_class, get_curve_class
+from datasets.tabular import TabularModel, TabularModelPerturb, TabularModelCurve
 from style import bold
 from multiprocessing import set_start_method
 from joblib import Parallel, delayed
@@ -67,7 +66,7 @@ def _load_config(config_file, name):
     return config[name]
 
 def _load_model(idx):
-    """Load model from globals (model_class, model_args, directory, mode_perturb_args, ts)"""
+    """Load model from globals (model_args, directory, mode_perturb_args, ts)"""
     # Load perturbed model (implement perturbations for mode connectivity)
     if mode_connect:
         model = curves.CurveNet(*curve_args)
@@ -78,23 +77,23 @@ def _load_model(idx):
                                            TabularModelPerturb,
                                            mode_perturb_args, ts=ts)
     else:
-        model = model_class(*model_args)
+        model = TabularModel(*model_args)
         state_dict = torch.load(f'{directory}/model_{idx}.pth')
         model.load_state_dict(state_dict)
         if perturb:
             model = TabularModelPerturb(model, n_weight_perturbations,
-                                        weight_sigmas, weight_layers)  # No FMNIST yet
+                                        weight_sigmas, weight_layers)
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     model = model.to(device)
     return model
 
-def _get_logits(model):
-    """Get logits from globals (model, model_class, X_test_full, mode_connect, perturb)"""
+def _get_logits(model, ):
+    """Get logits from globals (model, TabularModel, X_test_full, mode_connect, perturb)"""
     if mode_connect:
         if perturb:
             logits = model.compute_logits(X_test_full)
         else:
-            logits = model.compute_logits(X_test_full, model_class, ts).mean(axis=0)
+            logits = model.compute_logits(X_test_full, TabularModel, ts).mean(axis=0)
     else:
         if perturb:
             logits = model.forward(torch.FloatTensor(X_test_full)).detach().numpy().mean(axis=0)
@@ -107,7 +106,7 @@ def _get_grads(model):
         if perturb:
             grads = model.compute_gradients(X_test)
         else:
-            grads = model.compute_gradients(X_test, model_class, ts).mean(axis=0)
+            grads = model.compute_gradients(X_test, TabularModel, ts).mean(axis=0)
     else:
         if perturb:
             grads = model.compute_gradients(X_test, mean=True)
@@ -121,7 +120,7 @@ def _get_sg(model):
             sg = model.compute_gradients(noisy_x)
             sg = sg.reshape(n_input_perturbations, n_inputs, n_features).mean(axis=0)
         else:
-            sg = model.compute_gradients(noisy_x, model_class, ts).mean(axis=0)
+            sg = model.compute_gradients(noisy_x, TabularModel, ts).mean(axis=0)
             sg = sg.reshape(n_input_perturbations, n_inputs, n_features).mean(axis=0)
     else:
         if perturb:
@@ -158,7 +157,7 @@ if __name__ == '__main__':
     print(bold("Dataset:"), name)
     trainset, testset = datasets.load_dataset(name)
     X_test_full, y_test_full = testset.data.numpy(), testset.labels.numpy()
-    if name in ['default', 'fmnist', 'gmsc', 'adult', 'heloc']:
+    if X_test_full.shape[0] > 1000:
         X_test, y_test = X_test_full[:1000], y_test_full[:1000]
         if exp != '':
             print("Computing explanation metrics for first 1000 test points")
@@ -182,11 +181,7 @@ if __name__ == '__main__':
         json.dump(config, f)
 
     # Determine model class and arguments
-    model_class = get_model_class(name)
-    if name == 'fmnist':
-        model_args = [10, dropout]
-    elif name in datasets._tabular_datasets:
-        model_args = [n_features, datasets.tabular.layers[name]]
+    model_args = [n_features, datasets.tabular.layers[name]]
 
     # Perturbation
     perturb = 'perturb_' if args['perturb'] else ''
@@ -202,8 +197,7 @@ if __name__ == '__main__':
         mode_connect = curve_type + '_'
         n_curve_samples = config['mode_connect']['n_curve_samples']
         ts = np.linspace(0, 1, n_curve_samples)
-        curve_class = get_curve_class(name)
-        curve_args = [_curve_dict[curve_type], curve_class, 2, n_features,
+        curve_args = [_curve_dict[curve_type], TabularModelCurve, 2, n_features,
                       datasets.tabular.layers[name], False, False]
         if perturb:
             n_pert_mode = config['mode_connect']['n_curve_perturbations']
